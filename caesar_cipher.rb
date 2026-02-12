f4e8335b8ffaf349845123018576ecea5b10787d
Kenneth Tan <kennethwytan6700@gmail.com>
2026-02-12 16:31:08 +0800

    Create the basis of ciphering up, down and punctuations/symbol letters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -14,12 +14,22 @@
 
 
 def caesar_cipher(string, int)
+  cipher_string = ""
   # str to int
   string.each_char do |char| 
-    if char.ord > 25
-      (char.ord + int) % 26
-    end
-  char.ord + int 
-
+    
+    # Checks for upcase letters
+    if char.ord >= 65 && char.ord <= 90
+      result = char.ord - 65
+      result + int 
+    # Checks for downcase letters
+    elsif char.ord >= 97 && char.ord <= 122
+      result = char.ord - 97
+      result + int
+    else    
+      result = char
+  end
+  # Concatenate letters to return the ciphered string
+  cipher_string += result
 end  
-  
+
