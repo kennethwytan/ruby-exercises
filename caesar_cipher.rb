96c4a975061adf432ba0c4d87cbad03aec4e47a3
Kenneth Tan <kennethwytan6700@gmail.com>
2026-02-19 14:23:09 +0800

    Completed caesar cipher project but issue with up and downcase, every character returned is in upcase. Will need to fix

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,17 +2,6 @@
 # replaced by a letter e.g with left shift of 3, D becomes 
 # A, E becomes B and so on
 
-
-#def string_to_number(string)
-#  string.to_i
-#end
-
-
-#def cap_first_word(word)
-#  word.capitalize
-#end
-
-
 def caesar_cipher(string, int)
   cipher_string = ""
   # str to int
@@ -20,16 +9,38 @@ def caesar_cipher(string, int)
     
     # Checks for upcase letters
     if char.ord >= 65 && char.ord <= 90
+      
+      # Converts character to number using .ord and -65 to get number between 0-25 (max to 25 to match the number of letters in the alphabet)
       result = char.ord - 65
-      result + int 
+      
+      # Wraps the number around past 25
+      result = (result + int) % 26
+
+      # Converts the number back to character
+      result = (result + 65).chr
+
     # Checks for downcase letters
     elsif char.ord >= 97 && char.ord <= 122
       result = char.ord - 97
-      result + int
+      result = (result + int) % 26
+      result = (result + 65).chr
     else    
       result = char
-  end
+    end
   # Concatenate letters to return the ciphered string
   cipher_string += result
-end  
+  end  
+  puts cipher_string
+end
+
+puts "Enter your sentence"
+
+sentence = gets.chomp
+
+puts "Enter the number of shift"
+
+number = gets.chomp.to_i
+
+caesar_cipher(sentence, number)
+
 
